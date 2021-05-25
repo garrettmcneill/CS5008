56ba9a5ea82ab2b0a42e5a2c599f63190c96c7d7
gmcneill <mcneill.g@northeastern.edu>
2021-05-24 22:38:41 -0400

    updated fib.c with test code

--- labs/lab2/fib.c
+++ labs/lab2/fib.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
 
-void update(int argument) {
-    argument = 10;
-}
+typedef struct student{
+    int id;
+    int age;
+}student_t;
 
 int main(){
-    int value = 5;
-    int* p_value = &value;
-
-    printf("Value is: %d\n", value);
-    printf("The address of value is: %p\n", p_value);
-    printf("The value at p_value is: %d\n", *p_value);
-
-    *p_value = 69;
-
-    update(value);
-    printf("The update value at p_value is: %d\n", *p_value);
-    printf("Value updated is: %d\n", value);
+    student_t s1;
+    student_t s2;
     return 0;
 }
