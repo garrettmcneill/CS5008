53ccc6ef709509483d2cf936bb21d7e1c8bcfaac
gmcneill <mcneill.g@northeastern.edu>
2021-05-24 22:44:47 -0400

    updated fib.c with test code

--- labs/lab2/fib.c
+++ labs/lab2/fib.c
@@ -5,6 +5,11 @@ typedef struct student{
     int age;
 }student_t;
 
+void printStudent(student_t aStudent){
+    printf("Student ID: %d, Age: %d\n",
+           aStudent.id, aStudent.age);
+}
+
 int main(){
     student_t s1;
     student_t s2;
@@ -22,7 +27,7 @@ int main(){
 
     s2.id = 1003;
 
-    printf("Student ID: %d, Age: %d\n", s2.id, s2.age);
+    printStudent(s2);
 
 
     return 0;
