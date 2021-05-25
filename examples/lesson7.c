afbe0b76c383df3354e77aa47f18e012606185ef
gmcneill <mcneill.g@northeastern.edu>
2021-05-24 23:36:21 -0400

    lesson7.c start

--- examples/lesson7.c
+++ examples/lesson7.c
@@ -4,6 +4,9 @@
 int main(){
     int numbers[3] = {1,2,3};
 
+    printf("sizeof(int): %d\n", sizeof(int));
+    printf("\n");
+
     printf("%p\n", &numbers[0]);
     printf("%p\n", &numbers[1]);
     printf("%p\n", &numbers[2]);
