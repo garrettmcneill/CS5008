55644fe46da533a3e9f7c0b281617873f477e3e0
gmcneill <mcneill.g@northeastern.edu>
2021-05-25 00:39:37 -0400

    lesson9.c in prog

---
+++ examples/singly_linked_list.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Node data structure for singly linked list.
+ */
+typedef struct node{
+    int data;
+    struct node* next;
+}node_t;
+
+/**
+ * Print nodes helper function.
+ * @param node
+ */
+void printNodes(node_t* node){
+    node_t itr = node;
+
+    while( itr != NULL ){
+        printf("%d", itr->data);
+        // set to next
+        itr = itr->next;
+    }
+    printf("\n");
+}
+
+int main(){
+    node_t node1;
+    node_t node2;
+    node_t node3;
+
+    node1.data = 1;
+    node2.data = 2;
+    node3.data = 3;
+
+    node1.next = &node2;
+    node2.next = &node3;
+    node3.next = NULL;
+
+    printNodes(&node1);
+
+    return 0;
+
+
+}
