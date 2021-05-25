f98963821102d84c199d1035eaffc93a856f7fe6
gmcneill <mcneill.g@northeastern.edu>
2021-05-25 00:47:54 -0400

    singly linked list in prog

--- examples/singly_linked_list.c
+++ examples/singly_linked_list.c
@@ -4,19 +4,19 @@
 /**
  * Node data structure for singly linked list.
  */
-typedef struct node{
+typedef struct node {
     int data;
-    struct node* next;
-}node_t;
+    struct node *next;
+} node_t;
 
 /**
  * Print nodes helper function.
  * @param node
  */
-void printNodes(node_t* node){
-    node_t* itr = node;
+void printNodes(node_t *node) {
+    node_t *itr = node;
 
-    while( itr != NULL ){
+    while (itr != NULL) {
         // print node data
         printf("%d ", itr->data);
         // set to next ...
@@ -25,20 +25,51 @@ void printNodes(node_t* node){
     printf("\n");
 }
 
-int main(){
-    node_t node1;
-    node_t node2;
-    node_t node3;
 
-    node1.data = 1;
-    node2.data = 2;
-    node3.data = 3;
+/**
+ * node constructor
+ * @param data - int data to store in a node
+ * @return a newNode of type node_t
+ */
+node_t *makeNode(int data) {
+    node_t* = newNode = (node_t *) malloc(sizeof(node_t));
+    if (newNode = NULL) {
+        return NULL;
+    }
+    newNode->data = data;
+    return newNode;
+}
 
+void freeNode(node_t *node) {
+    if (node == NULL) {
+        return;
+    }
+
+    free(node);
+
+}
+
+
+int main() {
+    // init nodes
+    node_t* node1 = makeNode(1);
+    node_t* node2 = makeNode(2);
+    node_t* node3 = makeNode(3);
+
+    // traverse
     node1.next = &node2;
     node2.next = &node3;
     node3.next = NULL;
 
+    // print
     printNodes(&node1);
+
+
+    // free them from the heap
+    freeNode(node1);
+    freeNode(node2);
+    freeNode(node3);
+
 
     return 0;
 
