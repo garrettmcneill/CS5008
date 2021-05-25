c64c65794bc5c0a097dc71b8d95a20e5d2a3a62d
gmcneill <mcneill.g@northeastern.edu>
2021-05-24 22:20:12 -0400

    updated fib.c with test code

--- labs/lab2/fib.c
+++ labs/lab2/fib.c
@@ -6,6 +6,7 @@ void update(int argument) {
 
 int main(){
     int value = 5;
+    int* p_value = &value;
     printf("Value is: %d\n", value);
     printf("The address of value is: %p\n", value);
     update(value);
