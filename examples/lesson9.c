5b8ae731962eb21958e96a011645f8dc2261c2dd
gmcneill <mcneill.g@northeastern.edu>
2021-05-25 00:12:49 -0400

    lesson9.c start

---
+++ examples/lesson9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Lesson 9.1 Dynamically Resized Arrays as a Data Structure - Constructor and Destructor
+
+typedef struct vector{
+    int* data;
+    int size;
+    int capacity;
+}vector_t;
+
+int main(){
+    return 0;
+}
