391a5978415e25911aa367eca97925c495ec31ae
gmcneill <mcneill.g@northeastern.edu>
2021-05-24 23:34:30 -0400

    lesson7.c start

---
+++ examples/lesson7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int numbers[3] = {1,2,3};
+
+    printf("%p\n", &numbers[0]);
+    printf("%p\n", &numbers[1]);
+    printf("%p\n", &numbers[2]);
+
+    return 0;
+}
