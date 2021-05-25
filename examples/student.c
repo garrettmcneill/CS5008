739afe13248eb78aee47f895df7a509123625f10
gmcneill <mcneill.g@northeastern.edu>
2021-05-24 22:57:29 -0400

    updated student.c with in-module example code

--- examples/student.c
+++ examples/student.c
@@ -1,2 +1,38 @@
 #include <stdio.h>
 
+typedef struct student{
+    int id;
+    int age;
+}student_t;
+
+void printStudent(student_t* aStudent){
+    printf("Student ID: %d, Age: %d\n",
+           (*aStudent).id, (*aStudent).age);
+}
+
+
+void updateStudent(student_t* aStudent, int age, int id){
+    (*aStudent).age = age;
+    (*aStudent).id = id;
+}
+
+
+int main(){
+    student_t s1;
+    student_t s2;
+
+    //let's assign some values to s1, s2
+
+    updateStudent(&s1, 23, 1001);
+    updateStudent(&s2, 24, 1002);
+
+    printStudent(&s1);
+    printStudent(&s2);
+
+    updateStudent(&s2, s2.age, 1003);
+
+    printStudent(&s2);
+
+
+    return 0;
+}
