0e1a40b6915268fef7ea26a1b374994b682670b4
gmcneill <mcneill.g@northeastern.edu>
2021-05-25 22:09:51 -0400

    update fib.c in prog

--- labs/lab2/fib.c
+++ labs/lab2/fib.c
@@ -3,12 +3,14 @@
 
 long fib(long n){
     // unsigned long array to store fib numbers up
-    long f[50];
+    long f[n];
     long i;
 
+    // where fib(0) = 0, fib (1) = 1
     f[0] = 0;
     f[1] = 1;
 
+    // loop through fib seq recursively
     for (i = 2; i <= n; i++){
         f[i] = f[i-1] + f[i-2];
     }
@@ -18,8 +20,8 @@ long fib(long n){
 
 int main(){
 
-    long n = 9;
-    printf("%20d", fib(n));
+    long n = 50;
+    printf("%20lu", fib(n));
     printf("\n");
     return 0;
 }
