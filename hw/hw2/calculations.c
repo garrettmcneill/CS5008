1409ce802c879b844c8ee88cc371649aa599e6f4
gmcneill <mcneill.g@northeastern.edu>
2021-05-26 00:21:02 -0400

    calculations.c first pass

--- hw/hw2/calculations.c
+++ hw/hw2/calculations.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
 
+int main(){
+
+    // Problem Inputs
+    double x = 10.0;
+    double y = 5.0;
+
+    // Calculations
+    double sum = x + y;
+    double difference = x - y;
+    double product = x * y;
+    double quotient = x/y;
+
+    // Print Statements
+    printf("Two Values:L %f and %f\n", x, y);
+    printf("Sum: %f\n", sum);
+    printf("Difference: %f\n", difference);
+    printf("Product: %f\n", product);
+    printf("Quotient: %f\n", quotient);
+
+    return 0;
+}
