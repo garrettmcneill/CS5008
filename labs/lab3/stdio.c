2501b76c18bac266363679a9190fd801de50c4d5
gmcneill <mcneill.g@northeastern.edu>
2021-05-27 23:55:56 -0400

    let's try an elif...

--- labs/lab3/stdio.c
+++ labs/lab3/stdio.c
@@ -27,7 +27,9 @@ int main() {
 
         if (('a' <= input[i]) && (input[i] <= 'z')) {
             printf("%c -- 0x%x\n", input[i] - 0x20, input[i] - 0x20);
-        } else {
+        } else if (('A' <= input[i]) && (input[i] <= 'Z')){
+            printf("%c -- 0x%x\n", input[i], input[i]);
+        } else{
             printf("  -- 0x20\n");
         }
 
