856e234df00102ce52ab6057898d61c49a03854d
gmcneill <mcneill.g@northeastern.edu>
2021-05-27 21:25:29 -0400

    calculations.c serially hardcoded values from prompt

--- hw/hw2/calculations.c
+++ hw/hw2/calculations.c
@@ -13,7 +13,7 @@ int main(){
     double quotient = x/y;
 
     // Print Statements
-    printf("--------- First Value Set -----------")
+    printf("--------- First Value Set -----------");
     printf("Two Values:L %f and %f\n", x, y);
     printf("Sum: %f\n", sum);
     printf("Difference: %f\n", difference);
@@ -23,7 +23,7 @@ int main(){
     // update x,y to (40, 8) ... print it again!
     x = 40;
     y = 8;
-    printf("\n--------- Second Value Set -----------")
+    printf("\n--------- Second Value Set -----------");
     printf("Two Values:L %f and %f\n", x, y);
     printf("Sum: %f\n", sum);
     printf("Difference: %f\n", difference);
@@ -33,7 +33,7 @@ int main(){
     // update x,y to (1, 4) ... print it again!
     x = 1;
     y = 4;
-    printf("\n--------- Third Value Set -----------")
+    printf("\n--------- Third Value Set -----------");
     printf("Two Values:L %f and %f\n", x, y);
     printf("Sum: %f\n", sum);
     printf("Difference: %f\n", difference);
@@ -43,7 +43,7 @@ int main(){
     // update x,y to (15, 30) ... print it again!
     x = 15;
     y = 30;
-    printf("\n--------- Fourth Value Set -----------")
+    printf("\n--------- Fourth Value Set -----------");
     printf("Two Values:L %f and %f\n", x, y);
     printf("Sum: %f\n", sum);
     printf("Difference: %f\n", difference);
