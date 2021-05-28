f62b5d3fcb5282283f36f1de4868fee8c2077c68
gmcneill <mcneill.g@northeastern.edu>
2021-05-27 21:52:23 -0400

    calculations.c serially hardcoded values, code cleanup

--- examples/wk3/makeUpper.c
+++ examples/wk3/makeUpper.c
@@ -8,7 +8,7 @@ int main() {
     for (i=0; i< strlen(input); i++) {
         printf("%c %.3d %x", input[i], input[i], input[i]);
 
-        if ( ('a') <= input[i]) && (input[i] <= 'z') ) {
+        if ( ('a' <= input[i]) && ( input[i] <= 'z') ) {
             printf("-- upper case %c\n", input[i]-0x20);
         } else {
             printf("\n");
