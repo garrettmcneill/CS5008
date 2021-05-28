57fe9b8225ae2098aceceb99b2937a2d469c378f
gmcneill <mcneill.g@northeastern.edu>
2021-05-27 21:57:41 -0400

    first rough pass lab3

--- labs/lab3/stdio.c
+++ labs/lab3/stdio.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int i;
-char input[50] = "abcdefghijklmnopqrstuvwxyz0123456789";
 
 int main() {
+
+    char input[140];
+    int i;
+
+    printf(\n"input a string (QUIT for done): ");
+    gets(input);
+
+
     for (i=0; i< strlen(input); i++) {
         printf("%c %.3d %x", input[i], input[i], input[i]);
 
