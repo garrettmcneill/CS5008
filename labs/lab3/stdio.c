8bb359a3856a6f3583f5f07eb5c713eded702dbe
gmcneill <mcneill.g@northeastern.edu>
2021-05-27 22:02:02 -0400

    first rough pass lab3

--- labs/lab3/stdio.c
+++ labs/lab3/stdio.c
@@ -12,7 +12,7 @@ int main() {
 
 
     for (i=0; i< strlen(input); i++) {
-        printf("%c %.3d %x", input[i], input[i], input[i]);
+        printf("%c: %.3d %x", input[i], input[i], input[i]);
 
         if ( ('a' <= input[i]) && ( input[i] <= 'z') ) {
             printf("-- upper case! %c\n", input[i]-0x20);
