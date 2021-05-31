23a2976f42fc72693b19cd7b028a33c254494eae
gmcneill <mcneill.g@northeastern.edu>
2021-05-31 15:48:31 -0400

    a room of highly trained monkeys typing random chars would have gotten something working quicker than me.

--- examples/wk3/strcmp_test.c
+++ examples/wk3/strcmp_test.c
@@ -2,24 +2,20 @@
 #include <string.h>
 
 
-
-char input[140];    /* use constants for array bounds */
+char input[140];    // declare input array with max chars
 
 
 int main(void) {
 
-
+    // do while scheme for quitting when user says to quit loosely based on
+    // https://stackoverflow.com/questions/55033405/type-quit-to-quit-c-program/55033679
     do {
         char c;     /* final character read */
         int retn;   /* variable to save scanf return */
         /* prompt */
         fputs("Enter a bunch of words ('quit' exits): ", stdout);
         /* read saving scanf return */
-        retn = scanf(" %199[^\n]%c", input, &c);
-        if (retn == EOF) {      /* check the return against EOF */
-            fputs("(user canceled input)\n", stderr);
-            return 0;
-        } else {
+
 
 
 
@@ -48,7 +44,7 @@ int main(void) {
             // print line break at end because prompt shows it
             printf("\\n  -- 0x20\n");
 
-        }
+
     } while ((strcmp(input, "quit") != 0) && (strcmp(input, "QUIT") != 0));
 
     return 0;
