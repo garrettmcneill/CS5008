f81615d1dd1eeb8697d4913b2fe40f0cb4545e73
gmcneill <mcneill.g@northeastern.edu>
2021-05-31 15:35:24 -0400

    test another approach

--- examples/wk3/strcmp_test.c
+++ examples/wk3/strcmp_test.c
@@ -15,52 +15,34 @@ int main(void) {
 
 
     do {
-        char c;     /* final character read */
-        int retn;   /* variable to save scanf return */
-        /* prompt */
-        fputs("Enter a bunch of words ('quit' exits): ", stdout);
-        /* read saving scanf return */
-        retn = scanf(" %199[^\n]%c", input, &c);
-        if (retn == EOF) {      /* check the return against EOF */
-            fputs("(user canceled input)\n", stderr);
-            return 0;
-        } else if (retn < 2) {    /* checking both string and c read */
-            fputs("input failure.\n", stderr);
-            empty_stdin();
-        } /* else if (c != '\n') {
-            fprintf(stderr, "warning: input exceeds %d characters.\n",
-                    140 - 1);
-            empty_stdin();
-        }*/ else {  /* good input, output string */
 
 
+        // parse input for chars
+        int i;
+        for (i = 0; i < strlen(input); i++) {
 
-            // parse input for chars
-            int i;
-            for (i = 0; i < strlen(input); i++) {
-
-                // parse & print lowercase chars
-                if (('a' <= input[i]) && (input[i] <= 'z')) {
-                    printf("%c -- 0x%x\n", input[i] - 0x20, input[i] - 0x20);
-                }
-                    // PARSE & PRINT UPPERCASE CHARS
-                else if (('A' <= input[i]) && (input[i] <= 'Z')) {
-                    printf("%c -- 0x%x\n", input[i], input[i]);
-                }
-                    // parse & print punctuation!?
-                else if (('!' <= input[i]) && (input[i] <= '?')) {
-                    printf("%c -- 0x%x\n", input[i], input[i]);
-                }
-                    // parse blank spaces
-                else {
-                    printf("  -- 0x20\n");
-                }
-
+            // parse & print lowercase chars
+            if (('a' <= input[i]) && (input[i] <= 'z')) {
+                printf("%c -- 0x%x\n", input[i] - 0x20, input[i] - 0x20);
             }
-            // print line break at end because prompt shows it
-            printf("\\n  -- 0x20\n");
+                // PARSE & PRINT UPPERCASE CHARS
+            else if (('A' <= input[i]) && (input[i] <= 'Z')) {
+                printf("%c -- 0x%x\n", input[i], input[i]);
+            }
+                // parse & print punctuation!?
+            else if (('!' <= input[i]) && (input[i] <= '?')) {
+                printf("%c -- 0x%x\n", input[i], input[i]);
+            }
+                // parse blank spaces
+            else {
+                printf("  -- 0x20\n");
+            }
 
         }
+        // print line break at end because prompt shows it
+        printf("\\n  -- 0x20\n");
+
+
     } while ((strcmp(input, "quit") != 0) && (strcmp(input, "QUIT") != 0));
 
     return 0;
