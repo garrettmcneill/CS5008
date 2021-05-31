59bcb4ad66b3a27094f96eb1d719e2d0c04ff18c
gmcneill <mcneill.g@northeastern.edu>
2021-05-31 14:07:23 -0400

    let's try an elif...

--- labs/lab3/stdio.c
+++ labs/lab3/stdio.c
@@ -7,8 +7,24 @@ int main() {
     char input[140];
     int i;
 
-    printf("\ninput a string (QUIT for done): ");
-    fgets(input);
+    do {
+        printf("\ninput a string (QUIT for done): ");
+        gets(input);
+
+        for (i = 0; i < strlen(input); i++) {
+
+            if (('a' <= input[i]) && (input[i] <= 'z')) {
+                printf("%c -- 0x%x\n", input[i] - 0x20, input[i] - 0x20);
+            } else if (('A' <= input[i]) && (input[i] <= 'Z')){
+                printf("%c -- 0x%x\n", input[i], input[i]);
+            } else{
+                printf("  -- 0x20\n");
+            }
+
+        }
+
+
+    } while (j > 0);
 
 /*
     for (i=0; i< strlen(input); i++) {
@@ -23,17 +39,8 @@ int main() {
 */
 
 
-    for (i = 0; i < strlen(input); i++) {
 
-        if (('a' <= input[i]) && (input[i] <= 'z')) {
-            printf("%c -- 0x%x\n", input[i] - 0x20, input[i] - 0x20);
-        } else if (('A' <= input[i]) && (input[i] <= 'Z')){
-            printf("%c -- 0x%x\n", input[i], input[i]);
-        } else{
-            printf("  -- 0x20\n");
-        }
 
-    }
 
     return 0;
 }
