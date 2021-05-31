3babf8e7e6172ad4c9140bf1ae88be753ac57eca
gmcneill <mcneill.g@northeastern.edu>
2021-05-31 14:59:10 -0400

    test

--- examples/wk3/strcmp_test.c
+++ examples/wk3/strcmp_test.c
@@ -2,14 +2,15 @@
 #include <string.h>
 
 //stack overflow https://stackoverflow.com/questions/20738324/how-to-stop-a-program-based-on-users-input-in-c/20739382
+
 int main()
 {
     int a;
     int b;
     char c[5] = {'\0'};
-
-    do {
-        printf("Enter the two values you like to compare, type stop to end.\n");
+    printf("Enter the two values you like to compare, type stop to end.\n");
+    while (strcmp(c,"stop"))
+    {
         scanf("%d%d%s", &a, &b, c);
         if (!(a^b))
         {
@@ -22,7 +23,6 @@ int main()
             getchar();
         }
     }
-    while (strcmp(c,"stop"));
     printf("Thanks for playing.");
     getchar();
     return 0;
