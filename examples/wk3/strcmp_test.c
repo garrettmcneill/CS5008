5afcfb599fc1b05dd251c87d50eaf5ae07b0ffa3
gmcneill <mcneill.g@northeastern.edu>
2021-05-31 16:01:43 -0400

    a room of highly trained monkeys typing random chars would have gotten something working quicker than me.

--- examples/wk3/strcmp_test.c
+++ examples/wk3/strcmp_test.c
@@ -15,34 +15,40 @@ int main(void) {
         /* read saving scanf return */
         retn = scanf(" %199[^\n]%c", input, &c);
 
-        // parse input for chars
-        int i;
-        for (i = 0; i < strlen(input); i++) {
+        if ((strcmp(input, "QUIT") == 0 || (strcmp(input, "quit")){
+            printf("end program \n");
+            return 0;
+        } else {
 
-            // parse & print lowercase chars
-            if (('a' <= input[i]) && (input[i] <= 'z')) {
-                printf("%c -- 0x%x\n", input[i] - 0x20, input[i] - 0x20);
+            // parse input for chars
+            int i;
+            for (i = 0; i < strlen(input); i++) {
+
+                // parse & print lowercase chars
+                if (('a' <= input[i]) && (input[i] <= 'z')) {
+                    printf("%c -- 0x%x\n", input[i] - 0x20, input[i] - 0x20);
+                }
+                    // PARSE & PRINT UPPERCASE CHARS
+                else if (('A' <= input[i]) && (input[i] <= 'Z')) {
+                    printf("%c -- 0x%x\n", input[i], input[i]);
+                }
+                    // parse & print punctuation!?
+                else if (('!' <= input[i]) && (input[i] <= '?')) {
+                    printf("%c -- 0x%x\n", input[i], input[i]);
+                }
+                    // parse blank spaces
+                else {
+                    printf("  -- 0x20\n");
+                }
+
             }
-                // PARSE & PRINT UPPERCASE CHARS
-            else if (('A' <= input[i]) && (input[i] <= 'Z')) {
-                printf("%c -- 0x%x\n", input[i], input[i]);
-            }
-                // parse & print punctuation!?
-            else if (('!' <= input[i]) && (input[i] <= '?')) {
-                printf("%c -- 0x%x\n", input[i], input[i]);
-            }
-                // parse blank spaces
-            else {
-                printf("  -- 0x20\n");
-            }
+            // print line break at end because prompt shows it
+            printf("\\n  -- 0x20\n");
 
         }
-        // print line break at end because prompt shows it
-        printf("\\n  -- 0x20\n");
-
     } while ((strcmp(input, "quit") != 0) && (strcmp(input, "QUIT") != 0));
 
-        printf("end program \n");
+        printf("end program 2\n");
 
 
 
