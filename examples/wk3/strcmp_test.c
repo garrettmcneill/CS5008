4a2f0c7e02d2f72175668a0fa73fa36acfcb2185
gmcneill <mcneill.g@northeastern.edu>
2021-05-31 15:01:35 -0400

    test another approach

--- examples/wk3/strcmp_test.c
+++ examples/wk3/strcmp_test.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
-//stack overflow https://stackoverflow.com/questions/20738324/how-to-stop-a-program-based-on-users-input-in-c/20739382
+//stack overflow https://stackoverflow.com/questions/55033405/type-quit-to-quit-c-program/55033679
 
 int main()
 {
-    int a;
-    int b;
-    char c[5] = {'\0'};
-    printf("Enter the two values you like to compare, type stop to end.\n");
-    while (strcmp(c,"stop"))
-    {
-        scanf("%d%d%s", &a, &b, c);
-        if (!(a^b))
-        {
-            printf("both are equal\n");
-            getchar();
+
+    do {
+        char c;     /* final character read */
+        int retn;   /* variable to save scanf return */
+        /* prompt */
+        fputs ("Enter a bunch of words ('quit' exits): ", stdout);
+        /* read saving scanf return */
+        retn = scanf (" %199[^\n]%c", string, &c);
+        if (retn == EOF) {      /* check the return against EOF */
+            fputs ("(user canceled input)\n", stderr);
+            return 0;
         }
-        else
-        {
-            printf("both are not equal\n");
-            getchar();
+        else if (retn < 2) {    /* checking both string and c read */
+            fputs ("input failure.\n", stderr);
+            empty_stdin();
         }
-    }
-    printf("Thanks for playing.");
-    getchar();
+        else if (c != '\n') {   /* check c is '\n', else string too long */
+            fprintf (stderr, "warning: input exceeds %d characters.\n",
+                     MAXC - 1);
+            empty_stdin();
+        }
+        else    /* good input, output string */
+            printf ("string: %s\n", string);
+
+    } while (strcmp (string,"quit") != 0);
+
     return 0;
 }
