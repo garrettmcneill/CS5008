396c54ff233928ae76b875a4ed6afdfed648ac8b
gmcneill <mcneill.g@northeastern.edu>
2021-07-04 13:00:11 -0400

    debugging steps...

--- labs/lab6/main.c
+++ labs/lab6/main.c
@@ -6,6 +6,8 @@
 
 #include "lab6.h"
 
+const char inputFileName[] = "city.dat";
+
 /*
  * Forward declaration for city names. Included in .c for local use only..
  */
@@ -26,8 +28,8 @@ int main() {
         exit(-1);
     }
 
-    // open file
-    FILE *cityFilePtr = fopen("city.dat", "r");
+    // open input file
+    FILE *cityFilePtr = fopen(inputFileName, "r");
     // test if file exists
     if (cityFilePtr == NULL) {
         printf("File not found!\n");
@@ -37,7 +39,6 @@ int main() {
     // read file, adding each city to linked list
     do {
         tmpInt = fscanf(cityFilePtr, "%s %s %d", fromCityInput, toCityInput, &interCityDistanceInput);
-
         if (tmpInt > 0 && tmpInt < 3) {
             printf("Line could not be read.\n");
         } else {
@@ -54,9 +55,11 @@ int main() {
                 }
             }
         }
-
     } while (tmpInt > 0);
 
+    // Close the file
+    fclose(cityFilePtr);
+    cityFilePtr = NULL;
 
     //////// Create an alphabetized list of cities for selection by the user ////////
     char **cityArray = sllist_to_array(cityListPtr);
@@ -72,15 +75,133 @@ int main() {
 
     qsort((void *) cityArray, cityArraySize, sizeof(char *), cityArrayComparator);
 
-    // print sorted array
+
+    //////// Build an adjacency matrix based on the city.dat datafile ////////
+    int row, col;
+
+    // allocate adjacency matrix
+    int **adjMatrix = calloc(sizeof(int *), cityArraySize);
+    for (row = 0; row < cityArraySize; ++row) {
+        adjMatrix[row] = calloc(sizeof(int), cityArraySize);
+    }
+
+    // init matrix vals to 0
+    for (row = 0; row < cityArraySize; ++row) {
+        for (col = 0; col < cityArraySize; ++col) {
+            adjMatrix[row][col] = 0;
+        }
+    }
+
+    // open input file
+    cityFilePtr = fopen(inputFileName, "r");
+
+    // test if file exists
+    if (cityFilePtr == NULL) {
+        printf("Input file not found!\n");
+        exit(-1);
+    }
+
+    // read file, adding each city pair to adjacency matrix
+    do {
+        tmpInt = fscanf(cityFilePtr, "%s %s %d", fromCityInput, toCityInput, &interCityDistanceInput);
+        if (tmpInt > 0 && tmpInt < 3) {
+            printf("Line could not be read.\n");
+        } else {
+            // determine from city index (row)
+            // determine to city index (col)
+            // store interCityDistance in adjacency[row][col]
+        }
+    } while (tmpInt > 0);
+
+    // Close the file
+    fclose(cityFilePtr);
+    cityFilePtr = NULL;
+
+
+    // display list of cities a->z and prompt user for origin city
+    // if 0... quit
     for (idx = 0; idx < cityArraySize; ++idx) {
-        printf("%15s  ---  %2d\n", cityArray[idx], idx);
+        printf("%15s  ---  %2d\n", cityArray[idx], idx + 1);
+    }
+    // display list of cities minus origin city and get destination city
+
+    // apply Dijkstra's algorithm to find shortest path & distance
+
+    // print results of Dijkstra's
+    // loop
+
+    // clean up array and adjacency matrix
+    if (cityArray != NULL) {
+        for (row = 0; row < cityArraySize; ++row) {
+            if (cityArray[row] != NULL) {
+                free(cityArray[row]);
+                cityArray[row] = NULL;
+            }
+        }
+        free(cityArray);
+        cityArray = NULL;
+    }
+    if (adjMatrix != NULL) {
+        for (row = 0; row < adjMatrix; ++row) {
+            if (adjMatrix[row] != NULL) {
+                free(adjMatrix[row]);
+                adjMatrix = NULL;
+            }
+        }
+        free(adjMatrix);
+        adjMatrix = NULL;
     }
 
 
-    //////// Build an adjacency matrix based on the city.dat datafile ////////
 
+    /* Binary search to find a target in a sorted array of strings
+     *  stringArray - a list of strings a->z
+     *  arraySize - the number of entries in the array
+     *  target - the string to be found
+     * Returns the index of the matching entry, or 0.
+     * */
+    int findInArray(char **stringArray, int arraySize, string target) {
+        int botIdx = 0;
+        int topIdx = arraySize - 1;
+        int midIdx, compVal;
 
+        // check parameters
+        if (stringArray == NULL) {
+            printf("findInArray: stringArray is NULL.\n");
+            return -1;
+        }
+        if (arraySize < 1) {
+            printf("findInArray: array size must be > 1.\n");
+            return -1;
+        }
+        if (target == NULL){
+            printf("findInArray: target cannot be NULL.\n");
+            return -1;
+        }
+
+        // todo check !equal! or out of range
+        if (strcmp(target, stringArray[botIdx]) < 0) {
+            printf("findInArray: target out of range");
+            return -1;
+        }
+        if (strcmp(target, stringArray[topIdx]) > 0) {
+            printf("findInArray: target out of range");
+            return -1;
+        }
+
+        while(topIdx - botIdx >= 2){
+            midIdx = (topIdx + botIdx) / 2;
+            compVal = strcmp(target, stringArray[midIdx]);
+            if(compVal == 0){
+                return midIdx;
+            } else if (compVal < 0){
+                topIdx = midIdx;
+            } else {
+                botIdx = midIdx;
+            }
+        }
+        return -1;
+    }
 
     //Display the cities from which to select using a number that your
     //program assigns from the alphabetized list of cities that is
