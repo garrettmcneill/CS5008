e61572cd66d978822dfcf995d0160db2fdc3d2aa
Garrett McNeill <mcneill.g@northeastern.edu>
2021-07-08 19:56:12 -0400

    added hw7 folder and fib.c code from previous lab

---
+++ hw/hw7/fib.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+long fib(long n){
+    // unsigned long array to store fib numbers up to n
+    long f[n];
+    long i;
+
+    // where fib(0) = 0, fib (1) = 1
+    f[0] = 0;
+    f[1] = 1;
+
+    // loop through fib seq recursively
+    for (i = 2; i <= n; i++){
+        f[i] = f[i-1] + f[i-2];
+    }
+    return f[n];
+}
+
+
+int main(){
+
+    // declare total fib numbers to generate
+    long n = 2;
+    // declare iterator
+    long itr;
+
+    for(itr = n; itr < n+50; itr++)
+        printf(" %20lu ", fib(itr));
+
+    printf("\n");
+    return 0;
+}
+
