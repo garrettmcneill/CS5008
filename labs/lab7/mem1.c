a9357a392464351c747f088bbbdc559e141ab65e
Garrett McNeill <mcneill.g@northeastern.edu>
2021-07-15 19:56:34 -0400

    added r7 starter code for lab 7

---
+++ labs/lab7/mem1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void square(int num) {
+	num = num * num;
+}
+
+int main() {
+	int x = 4;
+	square(x);
+	printf("%d\n", x);
+	return 0;
+}
+
---
+++ labs/lab7/str1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+	char str[];
+	
+	printf("hakuna matata!"); 	// this line should copy 
+					// "hakuna matata!"
+ 	                         	// into our char array
+	printf("%s\n", str);
+ 	                         			
+	// Anything else?
+}
+
