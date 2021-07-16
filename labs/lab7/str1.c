80e4421af4ce88ac7e7876efe139dfc7dc74199b
gmcneill <mcneill.g@northeastern.edu>
2021-07-15 20:23:11 -0400

    updated to have heap memory

--- labs/lab7/str1.c
+++ labs/lab7/str1.c
@@ -2,11 +2,24 @@
 #include <string.h>
 
 int main() {
+    // Stack memory
 	char str[15];
 
     strncpy(str, "hakuna matata!", 15);
 
 	printf("%s\n", str);
+
+    // Heap memory
+    char * strPtr;
+
+    strPtr = (char *) malloc(15);
+
+    strncpy(strPtr, "hakuna matata!", 15);
+
+    printf("%s\n", strPtr);
+
+    free(strPtr);
+    strPtr = NULL;
  	                         			
 	return 0;
 }
