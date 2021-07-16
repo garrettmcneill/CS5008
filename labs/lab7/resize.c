0273f44392c44df7a220af24d5155d2f2760fb90
gmcneill <mcneill.g@northeastern.edu>
2021-07-15 21:32:16 -0400

    resize.c first pass

--- labs/lab7/resize.c
+++ labs/lab7/resize.c
@@ -9,18 +9,18 @@ int main(){
 	// Allows user to specify the original array size, 
 	// stored in variable n1.
 	printf("Enter original array size:");
-	int n1=0;
+	int n1 = 0;
 	scanf("%d",&n1);
 
 	// Create a new array of n1 integers
-	int* a1 = /* Fill in*/;
+    int* a1 = calloc(sizeof(int), n1);
 	int i;
 	for(i=0; i<n1; i++){
 		// Set each value in a1 to 100
-		/*Fill in*/=100;
+		*(a1 + i) = 100;
 		
 		// Print each element out (to make sure things look right)
-		printf("%d ",/*Fill in*/ );
+		printf("%d ", *(a1 + i) );
 	}
 
 	// User specifies the new array size, stored in variable n2.
@@ -29,20 +29,22 @@ int main(){
 	scanf("%d",&n2);
 
 	//Dynamically change the array to size n2
-	a1 = /* Fill in*/;
+    int a2 = realloc(a1,n2);
+    a1 = NULL;
 
 	// If the new array is a larger size, set all new members to 0. 
 	// Reason: we do not want to use uninitialized variables.
 
-	/*
-	Fill
-	In
-	*/
+	if (n2 > n1){
+	    for (i = n1, i < n2, ++i){
+            *(a1 + i) = 0;
+	    }
+	}
 	
 
 	for(i=0; i<n2;i++){
 		//Print each element out (to make sure things look right)
-		printf("%d ",/* Fill in */);
+		printf("%d ", *(a2 + 1));
 	}
 	printf("\n");
 	
