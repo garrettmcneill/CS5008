5fd0f790e094e8c9bfff701fbd16f081b54975df
gmcneill <mcneill.g@northeastern.edu>
2021-07-15 20:58:41 -0400

    update for pointer arithmetic

--- labs/lab7/sort.c
+++ labs/lab7/sort.c
@@ -5,22 +5,22 @@
 // Sort an array A using insertion sort. 
 // Notice it is to be passed by reference.
 
-void sort(int A[], int n){
+void sort(int *A, int n){
 	int tmp;
 	int i;
 	int j;
 
-	for(i=1; i<n; i++){
-		tmp = A[i];
-		j=i-1;
+	for(i = 1; i<n; i++){
+		tmp = *(A+i);
+		j = i-1;
 		// For each element in A, 
 		// 	search for where it belong in the subarray 
 		// 	preceeding its current location
-		while(tmp<A[j] && j>=0){
-			A[j+1]=A[j];
+		while(tmp < *(A+j) && j >= 0){
+			*(A+j+1) = *(A+j);
 			j-=1;
 		}
-		A[j+1]=tmp;
+        *(A+j+1) = tmp;
 	}
 }
 
@@ -34,19 +34,19 @@ int main(){
 	scanf("%d",&n);
 
 	//Array array. Change this to become a dynamic array through malloc.
-	int array[n];
+    int *array = calloc(sizeof(int), n);
 
 	//Assign each element in the array a random number between 0 and 31,999
 	int i;
 	for (i=0; i<n; i++){
-		array[i]=rand()%32000; //This line assigns random numbers
+		*(array + i) = rand()%32000; //This line assigns random numbers
 	}
 
 	//Prints out the elements of the unsorted array
 	int x;
 	printf("The unsorted array is: ");
 	for (x=0; x<n; x++){
-		printf("%d ",array[x]);
+		printf("%d ", *(array + x) );
 	}
 	printf("\n");
 	
@@ -56,7 +56,7 @@ int main(){
 	//Print out the elements of the now (supposedly) sorted array.
 	printf("The sorted array is: ");
 	for (x=0; x<n; x++){
-		printf("%d ",array[x]);
+		printf("%d ", *(array + x) );
 	}
 	printf("\n");
 	return 0;
