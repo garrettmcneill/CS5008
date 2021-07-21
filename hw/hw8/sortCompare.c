6c5f3fedb76081fdcfc12aacaf9e361ca986d4eb
gmcneill <mcneill.g@northeastern.edu>
2021-07-20 20:47:20 -0400

    first pass of hw8

--- hw/hw8/sortCompare.c
+++ hw/hw8/sortCompare.c
@@ -1,12 +1,32 @@
 #include "sortCompare.h"
 
-int main ( int argc, char *argv[] ){
+int main(int argc, char *argv[]) {
 
-    if (argc < 2 ){
+    //Constants
+    int MAXVAL = 1024000;
+
+    if (argc < 2) {
         printf("Not enough arguments passed at creation of program ");
         return -1;
     }
 
-    atoi(argv[1]);
+    // convert argument string to int
+    int numElements = atoi( argv[1] );
+
+    // allocate array of size n from argument
+    int *sortArray = calloc(sizeof(int), numElements);
+
+    // Intialize random number generator
+    srand(12345);
+
+    for(idx = 0; idx < numElements; ++idx){
+        randInts[idx] = rand() % MAXVAL;
+    }
+
+    insertionSort(sortArray, numElements);
+
+    // free from memory
+    free(sortArray);
+    sortArray = NULL;
 
 }
