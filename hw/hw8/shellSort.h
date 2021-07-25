39bd45a886e21ee4cfba7bb2f018ba54c349a1dd
Garrett <garrett@garrettmcneill.com>
2021-07-25 17:13:56 -0400

    updated header file

--- hw/hw8/shellSort.h
+++ hw/hw8/shellSort.h
@@ -1,2 +1,12 @@
+#ifndef __SHELL_SORT_H
+#define __SHELL_SORT_H
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
+
+
+#endif
+
+void shellSort(int intArray[], int elements);
